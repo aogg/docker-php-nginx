f11a876e1ca094d5b42ae5c436baf936ef9f7a55
aogg <aogg@users.noreply.github.com>
2020-08-20 19:30:35 +0800

    Update cron-git-auto.sh

--- provision_bootstrap.d/cron-git-auto.sh
+++ provision_bootstrap.d/cron-git-auto.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 chmod +x ./script/git-auto.sh;
-if [ $GIT_HTTP_URL ];then
+if [ $GIT_URL ];then
 
-  docker-cronjob ${CRON_FREQ:-'* * * * *'}' application ./script/git-auto.sh $GIT_URL $GIT_BRANCH $SSH_KEY $SSH_FILE_NAME 2>&1 /docker.stdout'
+  docker-cronjob ${CRON_FREQ:-'* * * * *'}' application ./script/git-auto.sh $GIT_URL ${GIT_BRANCH:-master} $SSH_KEY $SSH_FILE_NAME 2>&1 /docker.stdout'
   
 fi
