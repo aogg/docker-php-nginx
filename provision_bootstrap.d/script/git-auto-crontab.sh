369c3bf2b39f16d7dfa3dd835d3f6174cb9b17f7
aogg <aogg@users.noreply.github.com>
2020-08-26 09:12:38 +0800

    Update git-auto-crontab.sh

--- provision_bootstrap.d/script/git-auto-crontab.sh
+++ provision_bootstrap.d/script/git-auto-crontab.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# 检测是否有其他git命令在更新
+git_num=cat /proc/*/cmdline |grep -a 'git-auto.sh' | wc -l;
+if [[ git_num > 2 ]];then
+    echo '检测有其他git命令在更新, '$git_num;
+    exit;
+fi
+
 source /opt/docker/provision/bootstrap.d/script/source.sh >> /docker.stdout 2>&1
 su -c '/opt/docker/provision/bootstrap.d/script/git-auto.sh "$GIT_URL" ${GIT_BRANCH:-master} ${GIT_DIR:-/app} "$SSH_KEY" $SSH_FILE_NAME ' application >> /docker.stdout 2>&1
