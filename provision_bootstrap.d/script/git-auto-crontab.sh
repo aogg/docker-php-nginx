74b0292e36a1219adff623611cb87468420d05f8
aogg <aogg@users.noreply.github.com>
2020-08-26 09:26:07 +0800

    Update git-auto-crontab.sh

--- provision_bootstrap.d/script/git-auto-crontab.sh
+++ provision_bootstrap.d/script/git-auto-crontab.sh
@@ -7,5 +7,6 @@ if [[ $git_num > 2 ]];then
     exit;
 fi
 
+chown application:application ${GIT_DIR:-/app}
 source /opt/docker/provision/bootstrap.d/script/source.sh >> /docker.stdout 2>&1
 su -c '/opt/docker/provision/bootstrap.d/script/git-auto.sh "$GIT_URL" ${GIT_BRANCH:-master} ${GIT_DIR:-/app} "$SSH_KEY" $SSH_FILE_NAME ' application >> /docker.stdout 2>&1
