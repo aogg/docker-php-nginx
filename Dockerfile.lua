d2495d39ea5bd8e1b054048142d30154bc4ffb3c
aogg <8998031+aogg@users.noreply.github.com>
2020-11-20 18:05:18 +0800

    nginx-lua

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -2,7 +2,7 @@
 #FROM webdevops/php:${TAG}
 FROM webdevops/php-nginx:7.2-alpine
 
-RUN apk add --no-cache --virtual .build-deps nginx-mod-http-lua nginx-lua
+RUN apk add --no-cache nginx-mod-http-lua
 
 # RUN apt-get update \ 
 #     && apt-get install -y --no-install-recommends libnginx-mod-http-lua \     
