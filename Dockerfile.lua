40d15fb9f2484010fabd413a8e89e218be494a88
aogg <8998031+aogg@users.noreply.github.com>
2020-11-20 18:13:28 +0800

    nginx-lua copy

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -4,6 +4,8 @@ FROM webdevops/php-nginx:7.2-alpine
 
 RUN apk add --no-cache nginx-mod-http-lua
 
+COPY ./nginx-conf.d/nginx-lua.conf /opt/docker/etc/nginx/nginx.conf
+
 # RUN apt-get update \ 
 #     && apt-get install -y --no-install-recommends libnginx-mod-http-lua \     
 #     && rm -rf /var/lib/apt/lists/*
