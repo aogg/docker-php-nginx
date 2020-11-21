4dcdc2db2d1e0c751cd1ff528dd3c3b9514c951f
aogg <aogg@users.noreply.github.com>
2020-11-21 23:05:04 +0800

    Update Dockerfile.lua

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -6,10 +6,10 @@ FROM webdevops/php-nginx:${TAG}
 
 RUN set -x \
     if [ -n "$(which apt)" ];then \
-      apt-get update \ 
-      && apt-get install -y --no-install-recommends libnginx-mod-http-lua \     
-      && rm -rf /var/lib/apt/lists/*; \
-    else if [ -n "$(which apk)" ];then \
+      apt-get update; \ 
+      apt-get install -y --no-install-recommends libnginx-mod-http-lua; \     
+      rm -rf /var/lib/apt/lists/*; \
+    else if [ -n "$(which apk)" ]; then \
       apk add --no-cache nginx-mod-http-lua; \
     fi
 
