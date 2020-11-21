13338d622b80a524a77f414802af8fc0a879da98
aogg <aogg@users.noreply.github.com>
2020-11-21 22:53:30 +0800

    Update Dockerfile.lua

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -1,11 +1,18 @@
-#ARG TAG=latest
+ARG TAG=latest
 #FROM webdevops/php:${TAG}
-FROM webdevops/php-nginx:7.2-alpine
+FROM webdevops/php-nginx:${TAG}
 
-RUN apk add --no-cache nginx-mod-http-lua
 
-COPY ./nginx-conf.d/nginx-lua.conf /opt/docker/etc/nginx/nginx.conf
 
-# RUN apt-get update \ 
-#     && apt-get install -y --no-install-recommends libnginx-mod-http-lua \     
-#     && rm -rf /var/lib/apt/lists/*
+RUN set -x \
+    if [[ `which apt` ]];then \
+      apt-get update \ 
+      && apt-get install -y --no-install-recommends libnginx-mod-http-lua \     
+      && rm -rf /var/lib/apt/lists/*; \
+    else if [[ `which apk` ]];then \
+      apk add --no-cache nginx-mod-http-lua; \
+    fi
+
+
+COPY ./nginx-conf.d/nginx-lua.conf /opt/docker/etc/nginx/nginx.conf    
+    
