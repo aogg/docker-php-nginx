9a664e80b82cbe62fd1525003fa023ad9966f217
aogg <aogg@users.noreply.github.com>
2021-10-21 14:20:03 +0800

    解决脚本错误

--- script/docker-build-dir.sh
+++ script/docker-build-dir.sh
@@ -2,15 +2,16 @@
 
 # 运行$1目录下的多个脚本
 
-if [ -z "$2"];then
+if [ -z "$2" ]; then
     exit;
 fi
 
 dirPath=$1;
-buildArgArr=(${2//\|/});
+buildArgArr=${2//;/$'\n'};
 
 
 cd $dirPath;
+
 for i in ${buildArgArr[@]}
 do
     . $i
