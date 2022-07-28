a54074b99817fa98c1a91f1e80fd40b2def6972e
aogg <aogg@users.noreply.github.com>
2022-07-28 16:31:15 +0800

    local-user

---
+++ service.d/supervisor.d/local-user.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+if [[ -n "$LOCAL_USER_ENV" ]];then
+
+    if [[ -n "$(id $LOCAL_USER_ENV)" ]];then
+
+        if [[ -n "$LOCAL_USER_NUM_ENV" ]];then
+            LOCAL_USER_NUM_ENV=$LOCAL_USER_ENV;
+        fi
+
+        userdel application;
+        addgroup -g $LOCAL_USER_NUM_ENV $LOCAL_USER_ENV;
+        #  adduser -S -D -u 1000 -h /var/cache/www -s /bin/sh -G www www;
+        adduser -S -D -u $LOCAL_USER_NUM_ENV -h /var/cache/$LOCAL_USER_ENV -s /bin/sh -G $LOCAL_USER_ENV $LOCAL_USER_ENV;
+    fi;
+fi;
