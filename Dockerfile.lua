7dc7d96c7ece6a2aa883a94c3b4e36f3783e403d
aogg <happy2007xinlan@sina.com>
2022-12-03 00:19:36 +0800

    改为openresty

--- Dockerfile.lua
+++ Dockerfile.lua
@@ -3,11 +3,21 @@ ARG TAG="php-nginx:latest"
 FROM adockero/${TAG}
 
 
-
+# docker-image-info dist-codename
+# 原有是最新nginx版本的
 RUN set -x \
     && if [ -n "$(which apt)" ]; then \
+      mv /etc/nginx /etc/nginx_bak; \
+      apt remove -y nginx; \
       apt-get update; \ 
-      apt-get install -y --no-install-recommends libnginx-mod-http-lua; \     
+      wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -; \
+      codename=`grep -Po 'VERSION="[0-9]+ \(\K[^)]+' /etc/os-release`;\
+      echo "deb http://openresty.org/package/debian $codename openresty" \
+      | tee /etc/apt/sources.list.d/openresty.list;\
+      apt-get update; \ 
+      apt-get install -y --no-install-recommends openresty; \     
+      rm -Rf /etc/nginx; \
+      mv /etc/nginx_bak; \
       rm -rf /var/lib/apt/lists/*; \
     elif [ -n "$(which apk)" ]; then \
       apk add --no-cache nginx-mod-http-lua; \
