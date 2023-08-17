be76cc17a6ebb36bd58774f41b2b82d71619eafc
aogg <aogg@users.noreply.github.com>
2023-08-17 16:37:56 +0800

    addgroup --system --gid $LOCAL_USER_NUM_ENV $LOCAL_USER_ENV;

--- service.d/supervisor.d/local-user.sh
+++ service.d/supervisor.d/local-user.sh
@@ -5,12 +5,15 @@ if [[ -n "$LOCAL_USER_ENV" ]];then
 
     if [[ -z "$(id $LOCAL_USER_ENV)" ]];then
 
+        userdel application;
         if [[ -z "$LOCAL_USER_NUM_ENV" ]];then
-            LOCAL_USER_NUM_ENV=$LOCAL_USER_ENV;
+            addgroup $LOCAL_USER_ENV;
+            LOCAL_USER_NUM_ENV=$LOCAL_USER_ENV
+        else
+            addgroup --system --gid $LOCAL_USER_NUM_ENV $LOCAL_USER_ENV;
         fi
 
-        userdel application;
-        addgroup -g $LOCAL_USER_NUM_ENV $LOCAL_USER_ENV;
+        # addgroup --system --gid $LOCAL_USER_NUM_ENV $LOCAL_USER_ENV;
         #  adduser -S -D -u 1000 -h /var/cache/www -s /bin/sh -G www www;
         adduser -S -D -u $LOCAL_USER_NUM_ENV -h /var/cache/$LOCAL_USER_ENV -s /bin/sh -G $LOCAL_USER_ENV $LOCAL_USER_ENV;
     fi;
