4c841ae43c180f5d57367eabc56a168efccb624d
aogg <8998031+aogg@users.noreply.github.com>
2024-09-05 21:20:36 +0800

    other

--- provision_entrypoint.d/30-local-php-fpm.sh
+++ provision_entrypoint.d/30-local-php-fpm.sh
@@ -4,5 +4,8 @@
 if [ -n "$FPM_LISTEN" ]; then
 
     sed -i -e "s%listen = 127.0.0.1:9000%listen = ${FPM_LISTEN}%g" /opt/docker/etc/php/fpm/pool.d/application.conf;
+    sed -i -e "s%listen = [::]:9000%listen = ${FPM_LISTEN}%g" /opt/docker/etc/php/fpm/pool.d/application.conf;
+
+    sed -i -e "s%listen = [::]:9000%listen = ${FPM_LISTEN}%g" /usr/local/etc/php-fpm.d/application.conf;
 
 fi
