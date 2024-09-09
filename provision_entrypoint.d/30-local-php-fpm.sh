02547aa6c444f6d4773a9bef97ab20b376724efc
aogg <8998031+aogg@users.noreply.github.com>
2024-09-09 15:48:29 +0800

    fpm 1

--- provision_entrypoint.d/30-local-php-fpm.sh
+++ provision_entrypoint.d/30-local-php-fpm.sh
@@ -2,10 +2,11 @@
 
 
 if [ -n "$FPM_LISTEN" ]; then
+    echo "替换fpm端口----$FPM_LISTEN";
 
     sed -i -e "s%listen = 127.0.0.1:9000%listen = ${FPM_LISTEN}%g" /opt/docker/etc/php/fpm/pool.d/application.conf;
-    sed -i -e "s%listen = [::]:9000%listen = ${FPM_LISTEN}%g" /opt/docker/etc/php/fpm/pool.d/application.conf;
+    sed -i -e "s%listen = \[::\]:9000%listen = ${FPM_LISTEN}%g" /opt/docker/etc/php/fpm/pool.d/application.conf;
 
-    sed -i -e "s%listen = [::]:9000%listen = ${FPM_LISTEN}%g" /usr/local/etc/php-fpm.d/application.conf;
+    sed -i -e "s%listen = \[::\]:9000%listen = ${FPM_LISTEN}%g" /usr/local/etc/php-fpm.d/application.conf;
 
 fi
